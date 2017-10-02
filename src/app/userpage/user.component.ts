7e88f23e1c51eda45db31eda23f5f5c9a87525ac
abdullaunais <muabdulla@outlook.com>
2017-10-02 17:17:26 +0530

    user profile page

--- src/app/userpage/user.component.ts
+++ src/app/userpage/user.component.ts
@@ -1,8 +1,47 @@
 import { Component } from '@angular/core';
+import { ObjectStorage } from '../utilities/object-storage';
+import { UserService } from '../services/user.service';
 
 @Component({
     selector: 'app-user-cmp',
-    templateUrl: 'user.component.html'
+    templateUrl: 'user.component.html',
+    providers: [UserService]
 })
 
-export class UserComponent {}
+export class UserComponent {
+    user: any = {};
+    addressList: any = {};
+
+    country: any;
+    province: any;
+    city: any;
+
+    isLoading: boolean = false;
+    isError: boolean = false;
+
+    constructor(
+        private storage: ObjectStorage,
+        private userService: UserService
+    ) {
+        this.user = this.storage.get('user.data');
+
+        this.country = this.storage.get('location.country');
+        this.province = this.storage.get('location.province');
+        this.city = this.storage.get('location.city');
+
+        this.initialize();
+    }
+
+    initialize() {
+        this.isLoading = true;
+        this.isError = false;
+        this.userService.getAddressList(this.user.userId, this.user.authToken).catch((err):any => {
+            this.isLoading = false;
+            this.isError = true;
+        }).subscribe(data => {
+            this.addressList = data
+            // this.locationLabel = this.city.nameEn + ", \n" + this.province.nameEn + ",\n" + this.country.nameEn + ".";
+            this.isLoading = false;
+        });
+    }
+}
--- src/app/userpage/user.routing.ts
+++ src/app/userpage/user.routing.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 import { UserComponent } from './user.component';
+import { AuthGuardService } from '../services/auth-guard.service';
 
 export const UserRoutes: Routes = [
     {
@@ -8,7 +9,8 @@ export const UserRoutes: Routes = [
       path: '',
       children: [ {
         path: 'pages/user',
-        component: UserComponent
+        component: UserComponent,
+        canActivate: [AuthGuardService]
     }]
 }
 ];
