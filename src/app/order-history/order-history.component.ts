1b65f27e05ab23856e821909a17084b247b2e038
abdullaunais <muabdulla@outlook.com>
2017-10-18 02:57:45 +0530

    feedback rating

--- src/app/order-history/order-history.component.ts
+++ src/app/order-history/order-history.component.ts
@@ -1,8 +1,13 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { ObjectStorage } from '../utilities/object-storage';
 import { DeliveryService } from '../services/delivery.service';
+import { OnClickEvent } from 'angular-star-rating';
+import { OnRatingChangeEven } from 'angular-star-rating';
+import { OnHoverRatingChangeEvent } from 'angular-star-rating';
+import { FormBuilder, Validators } from '@angular/forms';
 
 declare const $: any;
+declare const swal: any;
 
 @Component({
     selector: 'app-order-history',
@@ -10,16 +15,34 @@ declare const $: any;
     styleUrls: ['./order-history.component.scss']
 })
 export class OrderHistoryComponent implements AfterViewInit {
+    @ViewChild('ratingModal') ratingModal: ElementRef;
+
+    authToken: any;
+
     start: number = 0;
     offset: number = 5;
 
     orders: Array<any> = [];
     user: any = {};
     isLoading: boolean = false;
+
+    activeOrder: any = null;
+    rating: number = 0;
+    ratingComment: string = '';
+    onClickResult: OnClickEvent;
+    onHoverRatingChangeResult: OnHoverRatingChangeEvent;
+    onRatingChangeResult: OnRatingChangeEven;
+
+    public feedbackForm = this.formBuilder.group({
+        formComment: ['', [Validators.required]]
+    });
+
     constructor(
         private storage: ObjectStorage,
+        public formBuilder: FormBuilder,
         private deliveryService: DeliveryService
     ) {
+        this.authToken = this.storage.get('user.authToken');
         this.initialize();
     }
     initialize() {
@@ -39,8 +62,83 @@ export class OrderHistoryComponent implements AfterViewInit {
             }
             this.isLoading = false;
         });
-
     }
+
+    openRatingModal(order: any) {
+        console.log('ACTIVE ORDER: ', order);
+        this.activeOrder = order;
+        $('#ratingModal').modal('show');
+    }
+
+    validateFeedback() {
+        this.ratingComment = this.feedbackForm.value.formComment;
+        console.log('COMMENT -> ', this.ratingComment);
+
+        const reviewObj = {
+            'commentEn': this.ratingComment,
+            'commentAr': this.ratingComment,
+            'rating': this.rating,
+            'userId': this.activeOrder.orderHeader.userId,
+            'shopId': this.activeOrder.orderHeader.shopId
+        };
+
+        this.deliveryService.sendReview(reviewObj, this.authToken).catch((err): any => {
+            swal({
+                type: 'error',
+                title: 'Error!',
+                text: 'Feedback Saving Failed!',
+                buttonsStyling: false,
+                confirmButtonClass: 'btn btn-danger'
+            }).then(() => {
+
+            });
+        }).subscribe(res => {
+            if (res.code === 1) {
+                swal({
+                    type: 'success',
+                    title: 'Success!',
+                    text: 'Feedback Saved!',
+                    buttonsStyling: false,
+                    confirmButtonClass: 'btn btn-success'
+                }).then(() => {
+
+                });
+            } else {
+                swal({
+                    type: 'error',
+                    title: 'Error!',
+                    text: 'Feedback Saving Failed!',
+                    buttonsStyling: false,
+                    confirmButtonClass: 'btn btn-danger'
+                }).then(() => {
+
+                });
+            }
+        });
+
+
+
+        this.activeOrder = null;
+        $('#ratingModal').modal('hide');
+    }
+
+    onClick = ($event: OnClickEvent) => {
+        // console.log('onClick $event: ', $event);
+        this.onClickResult = $event;
+        this.rating = $event.rating;
+    }
+
+    onRatingChange = ($event: OnRatingChangeEven) => {
+        // console.log('onRatingUpdated $event: ', $event);
+        this.onRatingChangeResult = $event;
+        this.rating = $event.rating;
+    }
+
+    onHoverRatingChange = ($event: OnHoverRatingChangeEvent) => {
+        // console.log('onHoverRatingChange $event: ', $event);
+        this.onHoverRatingChangeResult = $event;
+    }
+
     ngAfterViewInit() {
         //  Activate the tooltips
         $('[rel="tooltip"]').tooltip();
--- src/app/order-history/order-history.module.ts
+++ src/app/order-history/order-history.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { OrderHistoryComponent } from "./order-history.component";
-import { OrderHistoryRoutes } from "./order-history.routing";
+import { OrderHistoryComponent } from './order-history.component';
+import { OrderHistoryRoutes } from './order-history.routing';
 import { DeliveryService } from '../services/delivery.service';
+import { StarRatingModule } from 'angular-star-rating';
 
 @NgModule({
     imports: [
         CommonModule,
         RouterModule.forChild(OrderHistoryRoutes),
-        FormsModule
+        ReactiveFormsModule,
+        StarRatingModule.forRoot()
     ],
     declarations: [OrderHistoryComponent],
     providers: [DeliveryService]
