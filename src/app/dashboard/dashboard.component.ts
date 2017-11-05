96dc08ed0f3f7b0fd19d9d5b85c5cdbfd47a9d08
abdullaunais <muabdulla@outlook.com>
2017-11-05 18:38:21 +0530

    dashboard changes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,7 +11,8 @@ declare const $: any;
 
 @Component({
     selector: 'app-dashboard',
-    templateUrl: './dashboard.component.html'
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit, AfterViewInit {
     counts: DashboardCounts = {
