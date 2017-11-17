3eb51c6b2a7040c80883291b8950cf2d57dc74e4
abdullaunais <muabdulla@outlook.com>
2017-11-17 07:41:51 +0530

    item card title overflow fix

--- src/app/services/config.ts
+++ src/app/services/config.ts
@@ -9,7 +9,7 @@ import 'rxjs/add/operator/map';
 */
 @Injectable()
 export class Config {
-  private readonly serverUrl = `https://beecme.com/delivery`;
+  private readonly serverUrl = `https://www.beecme.com/delivery`;
   //  private readonly serverUrl: string = `http://localhost:8080/delivery`;
   constructor() {
   
